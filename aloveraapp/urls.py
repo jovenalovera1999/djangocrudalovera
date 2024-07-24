6b8e2df47779218c53a781e976c573ea471b2cc7
unknown <DevHelloThere>
2024-07-24 14:45:23 +0800

    Gender CRUD done

--- aloveraapp/urls.py
+++ aloveraapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('genders', views.gender_index),
     path('gender/add', views.gender_create),
-    path('gender/store', views.gender_store)
+    path('gender/store', views.gender_store),
+    path('gender/edit/<int:gender_id>', views.gender_edit),
+    path('gender/update/<int:gender_id>', views.gender_update),
+    path('gender/delete/<int:gender_id>', views.gender_delete),
+    path('gender/destroy/<int:gender_id>', views.gender_destroy)
 ]
--- aloveraapp/views.py
+++ aloveraapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Gender
+from django.contrib import messages
 
 def gender_index(request):
   genders = Gender.objects.all()
@@ -16,5 +17,38 @@ def gender_create(request):
 def gender_store(request):
   gender = request.POST.get('gender')
   Gender.objects.create(gender=gender).save()
+  messages.success(request, 'Gender successfully saved.')
+  return redirect('/genders')
+
+def gender_edit(request, gender_id):
+  gender = Gender.objects.get(pk=gender_id)
+  
+  data = {
+    'gender': gender
+  }
+  
+  return render(request, 'gender/edit.html', data)
+
+def gender_update(request, gender_id):
+  genderObj = Gender.objects.get(pk=gender_id)
+  gender = request.POST.get('gender')
+  
+  genderObj.gender = gender
+  genderObj.save()
+  
+  return redirect('/genders')
+  
+def gender_delete(request, gender_id):
+  gender = Gender.objects.get(pk=gender_id)
+  
+  data = {
+    'gender': gender
+  }
+  
+  return render(request, 'gender/delete.html', data)
+
+def gender_destroy(request, gender_id):
+  genderObj = Gender.objects.get(pk=gender_id)
+  genderObj.delete()
   
   return redirect('/genders')
